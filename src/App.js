93e71fe65aa778d1c2ad511d45841dd05be17d1e
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-06-08 11:22:46 +0530

    i am working on this project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React ,{Component} from 'react'
 import NavBar from './components/NavBar'
 import NewItems from './components/NewsItems'
-
 export default class App extends Component {
   render() {
     document.body.style.backgroundColor='black'
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -5,13 +5,16 @@ export default class NewsItems extends Component {
     return (
 
       <>
-        <div className="card" style="width: 18rem;">
+        {/* <div className="card" style="width: 18rem;">
           <img src="..." className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="/" className="btn btn-primary">Go somewhere</a>
             </div>
+        </div> */}
+        <div className="card">
+          hello this is box
         </div>
       </>
 
