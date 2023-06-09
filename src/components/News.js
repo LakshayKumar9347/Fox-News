99eab1c4c95f38679877c504e3833d9404686a6b
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-06-09 19:42:04 +0530

    adding api

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import NewItems from './NewsItems'
 
 export default class News extends Component {
-  articles=[
+  articles = [
     {
       "source": {
-          "id": null,
-          "name": "Livemint"
+        "id": null,
+        "name": "Livemint"
       },
       "author": "Ankit Gohel",
       "title": "RBI Monetary Policy Live: MPC decides to keep policy repo rate unchanged at 6.5% | Mint - Mint",
@@ -15,11 +15,11 @@ export default class News extends Component {
       "urlToImage": "https://www.livemint.com/lm-img/img/2023/06/07/600x338/shaktikantarbiani_1680755770535_1686135849578.jpg",
       "publishedAt": "2023-06-08T04:38:26Z",
       "content": "The Reserve Bank of Indias (RBI) Monetary Policy Committee (MPC) will announce its policy decision today. The second bi-monthly monetary policy meeting of FY24 was held from June 6 to 8 and its outco… [+20877 chars]"
-  },
-  {
+    },
+    {
       "source": {
-          "id": null,
-          "name": "Hindustan Times"
+        "id": null,
+        "name": "Hindustan Times"
       },
       "author": "HT Tech",
       "title": "Unstable sunspot explodes, sparks BLACKOUTS on Earth; Solar storm in the offing? - HT Tech",
@@ -28,11 +28,11 @@ export default class News extends Component {
       "urlToImage": "https://images.hindustantimes.com/tech/img/2023/06/08/1600x900/sun-2224937_1280_1677654423439_1686197093584.jpg",
       "publishedAt": "2023-06-08T04:08:29Z",
       "content": "The National Oceanic and Atmospheric Administration (NOAA) reported on Monday that the high number of sunspots on the Earth-facing side of the Sun can become a concern as some of them could go unstab… [+2741 chars]"
-  },
-  {
+    },
+    {
       "source": {
-          "id": "the-times-of-india",
-          "name": "The Times of India"
+        "id": "the-times-of-india",
+        "name": "The Times of India"
       },
       "author": "TIMESOFINDIA.COM",
       "title": "‘Shameless’ - Adipurush director Om Raut gives goodbye kiss to Kriti Sanon inside Tirupati temple premises, gets criticised - Indiatimes.com",
@@ -41,11 +41,11 @@ export default class News extends Component {
       "urlToImage": "https://timesofindia.indiatimes.com/photo/100837568/size-50238/100837568.jpg",
       "publishedAt": "2023-06-08T04:07:02Z",
       "content": "Controversies never seem to end for the film Adipurush\r\n. In the latest, Kriti Sanon\r\n, Prabhas\r\n and director Om Raut\r\n visited the Tirupati temple\r\n to seek blessings ahead of their films release. … [+696 chars]"
-  },
-  {
+    },
+    {
       "source": {
-          "id": null,
-          "name": "NDTV News"
+        "id": null,
+        "name": "NDTV News"
       },
       "author": null,
       "title": "\"Our Lives At Stake\": Indian Students In Canada Face Deportation Risk - NDTV",
@@ -54,11 +54,11 @@ export default class News extends Component {
       "urlToImage": "https://c.ndtvimg.com/2023-06/5ff5o3ag_canada-students-_625x300_08_June_23.jpg",
       "publishedAt": "2023-06-08T03:47:00Z",
       "content": "The Punjab NRI Affairs Minister has sought the intervention of External Affairs Minister S Jaishankar.\r\nNew Delhi: Hundreds of Indian students in Canada have hit the streets in protest against the po… [+3512 chars]"
-  },
-  {
+    },
+    {
       "source": {
-          "id": null,
-          "name": "Hindustan Times"
+        "id": null,
+        "name": "Hindustan Times"
       },
       "author": "HT Entertainment Desk",
       "title": "Harman Baweja explains what happened on family ‘vacation’ post Love Story 2050 - Hindustan Times",
@@ -67,31 +67,25 @@ export default class News extends Component {
       "urlToImage": "https://www.hindustantimes.com/ht-img/img/2023/06/08/1600x900/love_story_2050_1686192493829_1686192499190.jpg",
       "publishedAt": "2023-06-08T03:35:52Z",
       "content": "Actor Harman Baweja of Love Story 250 fame is back with his new web series Scoop. He doesn't remind people of Hrithik Roshan anymore the way he was compared during his early days and sports a moustac… [+2184 chars]"
-  }
+    }
   ]
-  constructor(){
+  constructor() {
     super()
-    this.state={
-      articles:this.articles,
-      count :0
+    this.state = {
+      articles: this.articles,
+      count: 0
     }
   }
   render() {
-    console.log(this.state.articles);
     return (
-    <>
-    <div className="row m-5">
-      <div className="col-md-4">
-      <NewItems />
-      </div>
-      <div className="col-md-4">
-      <NewItems />
-      </div>
-      <div className="col-md-4">
-      <NewItems />
-      </div>
-    </div>
-    </>
+      <>
+        <div className="row m-5">
+          {this.articles.map((e)=>{return <div className="col-md-4">
+            <NewItems title={this.articles.title}/>
+          </div>}) }
+
+        </div>
+      </>
     )
   }
 }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItems extends Component {
   render() {
-    let {title,desc,url} = this.props
+    let {title,description,urlToImage,newsUrl} = this.props
     return (
-      // api key = 'dd30967d1d854b799d75da5a94a311c2'
       <>
         <div className="card" style={{width: "18rem"}}>
-          <img src={url} className="card-img-top" alt="..." />
+          <img src={urlToImage} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">{desc}</p>
-            <a href="/" className="btn btn-primary">Load More</a>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} className="btn btn-primary">Read More</a>
           </div>
         </div>
       </>
