38bec004f2e2b4836412a3de03f6725f56366f0c
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-06-12 15:05:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import './App.css';
-import React ,{Component} from 'react'
+import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News'
 export default class App extends Component {
   render() {
-    document.body.style.backgroundColor='#534750'
+    document.body.style.backgroundColor = '#534750'
     return (
       <>
         <NavBar />
+        <h1 className='text-light text-center m-3'>Bumblebee:- Breaking News and Updates</h1>
         <News />
       </>
     )
