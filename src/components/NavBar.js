2ddb3502a090b1fd30df7e51d32ed5a051a3fef1
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-06-13 11:57:44 +0530

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,29 +3,29 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
     render() {
         return (
-<div>
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-        <img className="navbar-brand " style={{ width: '2rem' }} alt='Not Found' src="./favicon.ico" />
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">BUSINESS</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/">INVESTING</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/">TECH</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-</div>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <img className="navbar-brand " style={{ width: '2rem' }} alt='Not Found' src="./favicon.ico" />
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/">BUSINESS</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">INVESTING</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">TECH</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import NewItems from './NewsItems'
 export default class News extends Component {
-    // articles = [
-    // here will the sampleData.json
-    // ]
+    // articles = [here will the sampleData.json]
     constructor() {
         super()
         this.state = {
@@ -11,38 +9,44 @@ export default class News extends Component {
             loading: false
         }
     }
-    // ! Code to fetch Api from server
+    //todo  Code to fetch Api from server
     async componentDidMount() {
         let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=dd30967d1d854b799d75da5a94a311c2'
-        let data = await fetch(url)
-        let parsedData = await data.json()
+        let data =await fetch(url)
+        let parsedData = await data.json() //todo  this is correct  ! this is not correct X parsedData = (await data).json
         console.log(parsedData);
         this.setState({ articles: parsedData.articles })
-    }
-    render() {
-        return (
-            <>
-                <div className="container row" style={{ width: "80%", margin: "2rem auto auto auto" }}>
-                    <h1 className='text-light  m-3'>News - Top Headlines</h1>
-                    {/* //! this is to render loading spinner */}
-                   {this.state.loading===true? <div className="container"  style={{ width: "92%", margin: "2rem auto auto auto" }}>
-                        <div className="d-flex align-items-center text-light">
-                            <strong>Loading...</strong>
-                            <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-                        </div>
-                    </div>:undefined}
-                    {/* //! this to render card */}
-                    {this.state.articles.map((e) => {
-                        return <div className="col-lg-4 col-md-6 p-3" key={e.url} >
-                            <NewItems title={e.title.substring(0, 70)} description={e.description ? e.description.substring(0, 140) : ""} urlToImage={e.urlToImage ? e.urlToImage : "https://www.destructoid.com/wp-content/uploads/2023/06/Foamstars_LogoLead_061023.jpg"} newsUrl={e.url} />
-                        </div>
-                    })}
-                    <div className="container my-5 d-flex justify-content-between">
-                        <button type="button" className="btn btn-dark"> &larr; Previous </button>
-                        <button type="button" className="btn btn-dark"> Next &rarr; </button>
-                    </div>
-                </div>
-            </>
-        )
-    }
+        }
+        handlePrevClick = function () {
+            console.log('prev');
+        }
+        handleNextClick = function () {
+            console.log('next');
+        }
+render() {
+return (
+    <>
+<div className="container row" style={{ width: "80%", margin: "2rem auto auto auto" }}>
+    <h1 className='text-light  m-3'>News - Top Headlines</h1>
+    {/* //! this is to render loading spinner */}
+    {this.state.loading === true ? <div className="container" style={{ width: "92%", margin: "2rem auto auto auto" }}>
+        <div className="d-flex align-items-center text-light">
+            <strong>Loading...</strong>
+            <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+        </div>
+    </div> : undefined}
+    {/* //! this to render card */}
+    {this.state.articles.map((e) => {
+        return <div className="col-lg-4 col-md-6 p-3" key={e.url} >
+            <NewItems title={e.title.substring(0, 70)} description={e.description ? e.description.substring(0, 140) : ""} urlToImage={e.urlToImage ? e.urlToImage : "https://www.destructoid.com/wp-content/uploads/2023/06/Foamstars_LogoLead_061023.jpg"} newsUrl={e.url} />
+        </div>
+    })}
+    <div className="container my-5 d-flex justify-content-between">
+        <button type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous </button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}> Next &rarr; </button>
+    </div>
+</div>
+    </>
+)
+}
 }
